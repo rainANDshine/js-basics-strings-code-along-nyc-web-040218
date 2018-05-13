6025f8411c4aeb16658098bcbcb095c6a4ca220e
Shun Yao <ysmiracle@hotmail.com>
2018-05-13 01:43:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,4 @@
 const currentUser = 'Shun';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
